f6bd9e746bb0873119f60b52c03fb53d985fb40c
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2019-02-19 23:42:32 +0000

    Started Game
    -Started developing game
    -Added character model
    - Added Character idle animation
    - Added Character walking animation
    - Started work on hitboxes

---
+++ Assets/Scripts/PlayerOneFightScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneFightScript : MonoBehaviour
+{
+    // Declare Variables
+    private CharacterController controller;
+    private Vector3 movementVector;
+    private float verticalVelocity;
+    private Animator Player1Anim;
+    int PunchHash = Animator.StringToHash("Punch");
+    public Collider[] attackHitBoxes;
+   
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Player1Anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            StartAttack(attackHitBoxes[0]);
+            Player1Anim.SetTrigger(PunchHash);
+
+        }
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            StartAttack(attackHitBoxes[1]);
+        }
+        if(controller.isGrounded)
+        {
+            verticalVelocity = -1;
+            // If the player has pressed space, let them jump.
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+               
+                verticalVelocity = 10;
+            }
+        }
+        else
+        {
+            verticalVelocity -= 14 * Time.deltaTime;
+        }
+        //Initialise movementVector
+        movementVector = Vector3.zero;
+
+        //Movement for the game
+        // Check if the player is attempting to move to the left or right and if so, go that way. If not, stay still.
+        movementVector.x = Input.GetAxis("Horizontal") * 3;
+
+        // Lets the animator know if the player is moving and uses the appropriate animation
+        Player1Anim.SetFloat("Speed", movementVector.x);
+        // This is the players vertical movement. This makes it so the player is effected by gravity and can jump.
+        movementVector.y = verticalVelocity;
+
+        //Below line actually lets player move. DANNY DONT FORGET THIS LINE IN FUTURE!!!!!
+        controller.Move(movementVector * Time.deltaTime);
+        
+    }
+
+    private void StartAttack (Collider collider)
+    {
+        Collider[] colliders = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, collider.transform.rotation, LayerMask.GetMask("HitBox"));
+        foreach (Collider c in colliders)
+        {
+            if (c.transform.root == transform)
+                continue;
+
+            Debug.Log(c.name);
+        }
+            
+    }
+	
+	
+}
