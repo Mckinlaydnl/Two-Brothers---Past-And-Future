9d6db7a54cd266d32e51155db929779ba6d0089f
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2019-05-03 10:53:05 +0100

    Worked on combos
    Started fine tuning my combos even more. They do not work right now, however I am confident that once fixed, they will be perfect.

--- Assets/Scripts/PlayerOneFightScript.cs
+++ Assets/Scripts/PlayerOneFightScript.cs
@@ -9,22 +9,23 @@ public class PlayerOneFightScript : MonoBehaviour
     private Vector3 movementVector;
     private float verticalVelocity;
     private Animator Player1Anim;
-    int PunchHash = Animator.StringToHash("Punch");
-    int KickHash = Animator.StringToHash("Kick");
     int DashHash = Animator.StringToHash("IsRunning");
     public Collider[] attackHitBoxes;
     private float timeBetweenAttacks = 0.3f;
     private float attackTimer;
-    
+    int noOfButtonPresses; //Determines Which Animation Will Play
+    bool canPressButton; //Locks ability to press button during animation event
 
     private void Start()
     {
         controller = GetComponent<CharacterController>();
         Player1Anim = GetComponent<Animator>();
 
+        noOfButtonPresses = 0;
+        canPressButton = true;
     }
 
-  
+
     private void Update()
     {
         // Locks the player on the z-Axis
@@ -36,23 +37,20 @@ public class PlayerOneFightScript : MonoBehaviour
         if (Time.time >= attackTimer && (Input.GetKeyDown(KeyCode.O)))
         {
             StartAttack(attackHitBoxes[0]);
-            Player1Anim.SetTrigger(PunchHash);
-            attackTimer = Time.time + timeBetweenAttacks;
-
+            ComboStarter();
         }
         if (Time.time >= attackTimer && (Input.GetKeyDown(KeyCode.P)))
         {
             StartAttack(attackHitBoxes[1]);
-            Player1Anim.SetTrigger(KickHash);
-            attackTimer = Time.time + timeBetweenAttacks;
+            ComboStarter();
         }
-        if(controller.isGrounded)
+        if (controller.isGrounded)
         {
             verticalVelocity = -1;
             // If the player has pressed W, let them jump.
             if (Input.GetKeyDown(KeyCode.W))
             {
-               
+
                 verticalVelocity = 10;
             }
         }
@@ -74,25 +72,25 @@ public class PlayerOneFightScript : MonoBehaviour
         // This is the players vertical movement. This makes it so the player is effected by gravity and can jump.
         movementVector.y = verticalVelocity;
 
-       
-       
 
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             // Dash whenever space button is pressed
-                Debug.Log("Dash");
-                Player1Anim.SetTrigger(DashHash);
-                movementVector.x = 0;
-                movementVector.x = Input.GetAxis("Horizontal") * 50;
-            }
-        
+            Debug.Log("Dash");
+            Player1Anim.SetTrigger(DashHash);
+            movementVector.x = 0;
+            movementVector.x = Input.GetAxis("Horizontal") * 50;
+        }
+
 
         //Below line actually lets player move. DANNY DONT FORGET THIS LINE IN FUTURE!!!!!
         controller.Move(movementVector * Time.deltaTime);
-        
+
     }
 
-    private void StartAttack (Collider collider)
+    private void StartAttack(Collider collider)
     {
         // Detects if the hitbox overlaps the enemy players hitbox.
         Collider[] colliders = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents, collider.transform.rotation, LayerMask.GetMask("HitBox"));
@@ -109,8 +107,64 @@ public class PlayerOneFightScript : MonoBehaviour
             // Tells the enemy that they have taken damage
             c.SendMessageUpwards("RecieveDamage", damage);
         }
-            
+
     }
-	
-	
+
+    void ComboStarter()
+    {
+        if (canPressButton)
+        {
+            noOfButtonPresses++;
+        }
+
+        if (noOfButtonPresses == 1)
+        {
+            Player1Anim.SetInteger("Animation", 31);
+        }
+    }
+
+    void ComboCheck()
+    {
+
+        canPressButton = false;
+
+        if (Player1Anim.GetCurrentAnimatorStateInfo(0).IsName("Punch") && noOfButtonPresses == 1)
+        {//If the first animation is still playing and only 1 button has been pressed, return to idle
+            Player1Anim.SetInteger("Animation", 4);
+            canPressButton = true;
+            noOfButtonPresses = 0;
+        }
+
+        if (Player1Anim.GetCurrentAnimatorStateInfo(0).IsName("Punch") && noOfButtonPresses >= 2)
+        {//If the first animation is still playing and at least 2 buttons has been pressed, continue the combo
+            Player1Anim.SetInteger("Animation", 33);
+            canPressButton = true;
+            noOfButtonPresses = 0;
+        }
+
+        if (Player1Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && noOfButtonPresses == 2)
+        {//If the second animation is still playing and only 2 buttons have been pressed, return to idle 
+            Player1Anim.SetInteger("Animation", 4);
+            canPressButton = true;
+            noOfButtonPresses = 0;
+        }
+
+        if (Player1Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 2") && noOfButtonPresses >= 3)
+        {//If the second animation is still playing and only 2 buttons have been pressed, continue the combo
+            Player1Anim.SetInteger("Animation", 6);
+            canPressButton = true;
+            noOfButtonPresses = 0;
+        }
+
+        if (Player1Anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 3"))
+        {//Since this is the last animation, just go back to idle
+            Player1Anim.SetInteger("Animation", 4);
+            canPressButton = true;
+            noOfButtonPresses = 0;
+        }
+
+
+    }
 }
+
+
